4aeb479102c3e51a314c012fbf17d9b01e0b3d3d
maedekeys <maeva.de.keyser@ulb.be>
2023-12-21 09:54:07 +0100

    added plotting

--- projet/serv.py
+++ projet/serv.py
@@ -2,7 +2,9 @@ import paho.mqtt.client as mqtt
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import base64
-
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 
 def decrypt(encrypted_text):
     iv_str, message = encrypted_text.split(';')
@@ -46,12 +48,48 @@ def write_to_file(data,file):
         f.write(data + '\n')
 
 # Callback when a message is received from the MQTT broker
+
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+axe1 = fig.add_subplot(1, 1, 1)
+
+# Set the maximum number of points to be displayed
+max_points = 10
+
+
+def animate(interval):
+    graph_data = open('filedata.txt', 'r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = map(int, line.split(','))
+            xs.append(x)
+            ys.append(y)
+
+    # Display only the last 'max_points' points
+    xs = xs[-max_points:]
+    ys = ys[-max_points:]
+
+    axe1.clear()
+    axe1.plot(xs, ys)
+
 def on_message(client, userdata, msg):
     topic = msg.topic
     msg = msg.payload.decode('utf-8')
     msg = decrypt(msg)
 
     print(f"Received message on topic {topic}: {msg}")
+
+    with open("filedata.txt", 'w+') as file:
+        # Write content to the file
+        x = len(file.readlines())
+        file.write(f"{x}, ',', {msg}\n")
+
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    plt.show()
 
 # Create an MQTT client instance
 client = mqtt.Client()
